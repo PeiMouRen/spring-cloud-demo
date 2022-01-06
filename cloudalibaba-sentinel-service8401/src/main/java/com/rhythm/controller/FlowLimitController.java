f8b4b5c00fd4f71ff4b2b18c68af7262591e0751
peixingzhe <peixingzhe@163.com>
2022-01-06 18:10:26 +0800

    sentinel test

--- cloudalibaba-sentinel-service8401/src/main/java/com/rhythm/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/rhythm/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.rhythm.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -40,6 +43,19 @@ public class FlowLimitController {
         int age = 10/0;
         log.info("testD 测试");
         return "testD";
+    }
+
+
+    @GetMapping("/hotKey")
+    @SentinelResource(value = "hotKey", blockHandler = "deal_hotKey")
+    public String hotKey(@RequestParam(value = "p1", required = false) String p1,
+                         @RequestParam(value = "p2", required = false) String p2) {
+        return "test hotKey";
+    }
+
+    public String deal_hotKey(String p1, String p2, BlockException exception) {
+        exception.printStackTrace();
+        return "hotKey is Block!!!";
     }
 
 }
